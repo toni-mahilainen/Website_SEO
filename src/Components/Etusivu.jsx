eb6192121b05be5a7b44db32dd9d0c91fcf0b444
Toni Mahilainen <toni.mahilainen@student.careeria.fi>
2020-01-30 09:05:52 +0200

    Palautteen lähetys

--- src/Components/Etusivu.jsx
+++ src/Components/Etusivu.jsx
@@ -1,49 +1,17 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Carousel } from 'react-bootstrap';
-import lataus from '../Images/lataus.jpg';
-import lataus1 from '../Images/lataus1.jpg';
-import lataus2 from '../Images/lataus2.jpg';
 
 class Etusivu extends Component {
     render() {
         return (
             <main id="mainpage">
                 <section id="mainpageUpper">
-                    <Carousel bsPrefix="carousel">
-                        <Carousel.Item>
-                            <img className="d-block" src={lataus} alt="First slide" />
-                            <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Kuva 1</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img className="d-block" src={lataus1} alt="Second slide" />
-                            <Carousel.Caption>
-                                <h3>Second slide label</h3>
-                                <p>Kuva 2</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img className="d-block" src={lataus2} alt="Third slide" />
-                            <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>Kuva3</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    </Carousel>
-                </section>
-                <section id="mainpageLower">
-                    <div className="mainpageHeader">
+                    <div className="mainpageHeader subNav">
                         <h1>Tervetuloa SEO Kosken kotisivuille</h1>
                     </div>
-                    <div className="boxLinks">
-                        <a className="box" href="Yritys"><span>Yritys</span></a>
-                        <a className="box" href="Kahvila_Ravintola"><span>Kahvila/Ravintola</span></a>
-                        <a className="box" href="Huoltamo"><span>Huoltamo</span></a>
-                        <a className="box" href="Palaute"><span>Palaute</span></a>
-                    </div>
+                </section>
+                <section id="mainpageLower">
+                    
                 </section>
             </main>
         );
--- src/Components/Palaute.jsx
+++ src/Components/Palaute.jsx
@@ -1,7 +1,99 @@
 import React, { Component } from 'react';
 import '../App.css';
+import axios from 'axios';
 
 class Palaute extends Component {
+    constructor() {
+        super();
+        this.state = {
+            Name: "",
+            Email: "",
+            Subject: "",
+            Message: ""
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeInput = this.handleChangeInput.bind(this);
+        this.contactToBackend = this.contactToBackend.bind(this);
+        this.clearInputs = this.clearInputs.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        this.contactToBackend();
+    }
+
+    clearInputs() {
+        let inputs = document.getElementsByClassName("contactInput");
+
+        for (let index = 0; index < inputs.length; index++) {
+            inputs[index].value = "";
+        }
+    }
+
+    contactToBackend() {
+        let uri = "https://emailsendercoreseo.azurewebsites.net/api/email";
+
+        const messageObj = {
+            name: this.state.Name,
+            emailAddress: this.state.Email,
+            subject: this.state.Subject,
+            message: this.state.Message
+        };
+
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        };
+
+        axios.post(uri, messageObj, headers
+        ).then((response) => {
+            if (response.status >= 200 && response.status < 300) {
+                // alertin tilalle viesti käyttäjällä käyttöliittymään
+                alert("Email sent, awesome!");
+                this.clearInputs();
+            } else {
+                alert("Oops, something went wrong. Try again")
+            }
+        })
+    }
+
+    handleChangeInput(input) {
+        let inputField = input.target.id;
+        let nameValue = input.target.value;
+        let emailValue = input.target.value;
+        let subjectValue = input.target.value;
+        let messageValue = input.target.value;
+
+        switch (inputField) {
+            case "name":
+                this.setState({
+                    Name: nameValue
+                });
+                break;
+
+            case "email":
+                this.setState({
+                    Email: emailValue
+                });
+                break;
+
+            case "subject":
+                this.setState({
+                    Subject: subjectValue
+                });
+                break;
+
+            case "message":
+                this.setState({
+                    Message: messageValue
+                });
+                break;
+
+            default:
+                break;
+        }
+    }
+
     render() {
         return (
             <main id="contact">
@@ -11,19 +103,19 @@ class Palaute extends Component {
                     </div>
                 </section>
                 <section id="contactLower">
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <div id="formLeft">
                             Nimi<br />
-                            <input type="text" /><br />
+                            <input type="text" className="contactInput" id="name" onChange={this.handleChangeInput}/><br />
                             Sähköposti<br />
-                            <input type="text" /><br />
+                            <input type="text" className="contactInput" id="email" onChange={this.handleChangeInput}/><br />
                             Aihe<br />
-                            <input type="text" /><br />
+                            <input type="text" className="contactInput" id="subject" onChange={this.handleChangeInput}/><br />
                             <button type="submit">Lähetä</button>
                         </div>
                         <div id="formRight">
                             Viesti<br />
-                            <textarea></textarea><br />
+                            <textarea type="text" className="contactInput" id="message" onChange={this.handleChangeInput}></textarea><br />
                         </div>
                     </form>
                 </section>
